f0cc97498ca70560c6bf17c315e1e899ca0cd4c1
Test_name <babki2@gmx.de>
2014-09-19 21:47:10 +0200

    Excercises done up to 6

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void exchange(int&, int&);
+void ex_1()
+{
+    unsigned int number;
+    cout << "Number? ";
+    cout << endl;
+    if(number<=78 && number>=56)
+        cout<<"you win";
+    else
+        cout << "you lose";
+
+}
+
+void ex_2()
+{
+    //int number;
+    for(int i=8, number; i<=23; i++){
+        cout<<"Number? ";
+        cin >> number;
+        cout<< endl;
+    }
+}
+
+void ex_6(){
+    unsigned long n, sum;
+    sum=0;
+    cout<<"Number? ";
+    cin >>n;
+    cout <<endl <<"sum of cubes: ";
+    for(unsigned long i=5; i<=n; i++){
+        sum+=pow(i,3);
+    }
+    cout << sum << endl;
+}
 
 int main()
 {
-    int a,b;
-    cin >> a;
-    cin >> b;
-    //a=1; b=2;
-    exchange(a,b);
-    cout <<"a: "<< a << "b: "<< b  << endl;
-    return 0;
-}
-void exchange(int &x, int &y)
-{
-    int temp=x;
-    x=y;
-    y=temp;
-
+ex_6();
 
 }
+
